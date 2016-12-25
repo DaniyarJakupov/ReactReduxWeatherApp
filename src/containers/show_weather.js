f2c127fa9c4241cf1662f99447b0fbc0bb9db412
Daniyar <ddr199305@gmail.com>
2016-12-25 21:17:22 +0100

    Create a table for displaying weather

--- src/containers/show_weather.js
+++ src/containers/show_weather.js
@@ -2,27 +2,52 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 //==================================
 class ShowWeather extends Component {
+   renderWeather(cityData){
+      const name = cityData.city.name + ", " + cityData.city.country;
+      const temp = cityData.list[0].main.temp;
+      const pressure = cityData.list[0].main.pressure;
+      const humidity = cityData.list[0].main.humidity;
+      const description = cityData.list[0].weather[0].description;
+      return(
+         <tr key={name}>
+            <td>{name}</td>
+            <td>{temp}</td>
+            <td>{pressure}</td>
+            <td>{humidity}</td>
+            <td>{description  }</td>
+         </tr>
+      );
+   }
+
    render(){
-      if(!this.props.weather[0]){
-         return <div>EMPTY</div>
-      }
       return(
          <div>
             {console.log(this.props.weather)}
-            <h4>
-               The weather in {this.props.weather[0].city.name}, {this.props.weather[0].city.country} is currently&nbsp;
-               {this.props.weather[0].list[0].main.temp}°C with {this.props.weather[0].list[0].weather[0].description}
-            </h4>
+            <table className="table table-hover">
+               <thead>
+                  <tr>
+                     <th>City</th>
+                     <th>Temperature, °C</th>
+                     <th>Pressure, Pa</th>
+                     <th>Humidity, %</th>
+                     <th>Description</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  {this.props.weather.map(this.renderWeather)}
+               </tbody>
+            </table>
          </div>
       )
    }
 }
 
 function mapStateToProps(state){
+   // Whatever is returned  will show up as props
+   // inside of ShowWeather Class
    return{
       weather: state.weather
    }
 }
-
 //==================================
 export default connect(mapStateToProps)(ShowWeather);
